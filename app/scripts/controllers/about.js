d831a54292f0476f84e13c770097787a8b161eb0
Nicholas Bering <nick@whitemeadowsfarms.com>
2014-07-07 23:05:04 -0400

    Initialize AngularJS app with Yeoman.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name balrog30githubioApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the balrog30githubioApp
+ */
+angular.module('balrog30githubioApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name balrog30githubioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the balrog30githubioApp
+ */
+angular.module('balrog30githubioApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
